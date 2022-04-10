fa86dbacb599c336b3728be6f78e9749e7bedb2b
Boris Gonzalez <bgonzalez@byasystems.com.co>
2022-04-10 10:39:25 -0500

    avances de configuración

--- MsExample5/Services/AzureKeyVaultService.cs
+++ MsExample5/Services/AzureKeyVaultService.cs
@@ -1,11 +1,15 @@
-﻿using Azure.Identity;
+﻿using Azure.Core;
+using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace MsExample5.Services
@@ -41,11 +45,14 @@ namespace MsExample5.Services
             }
             else
             {
+
+                _client = new SecretClient(vaultUri: new Uri(keyVaultUrl), credential: new DefaultAzureCredential());
+
                 ////TODO: Imeplement
                 //// Way-2: Less Secure (For Developement scenario or Non Azure Service like On-Premise VM/Servers)
                 //// Connect to Azure Key Vault using the Client Id and Client Secret (AAD) - Get them from Azure AD Application.
-                //var keyVaultClientId = _configuration["AzureKeyVault:ClientId"];
-                //var keyVaultClientSecret = _configuration["AzureKeyVault:ClientSecret"];
+                var keyVaultClientId = _configuration["AzureKeyVault:ClientId"];
+                var keyVaultClientSecret = _configuration["AzureKeyVault:ClientSecret"];
                 //var credential = new ClientCredential(keyVaultClientId, keyVaultClientSecret);
                 //var authenticationContext = new AuthenticationContext($"https://login.microsoftonline.com/83a8a1a0-dfd1-43e6-976d-73889e9bc230");
                 //var result = authenticationContext.AcquireTokenAsync(uri, credential).Result;
@@ -54,11 +61,66 @@ namespace MsExample5.Services
                 //{
                 //    config.AddAzureKeyVault(keyVaultUrl, keyVaultClientId, keyVaultClientSecret, new DefaultKeyVaultSecretManager());
                 //}
-                _client = new SecretClient(vaultUri: new Uri(keyVaultUrl), credential: new DefaultAzureCredential());
+
+                //var keyVaultClient = new KeyVaultClient(async (authority, resource, scope) =>
+                //{
+                //    var aadCredential = new ClientCredential(keyVaultClientId, keyVaultClientSecret);
+                //    var authenticationContext = new AuthenticationContext(authority, null);
+                //    return (await authenticationContext.AcquireTokenAsync(resource, aadCredential)).AccessToken;
+                //});
+                //var client = new SecretClient(vaultUri: keyVaultClientId, credential: new DefaultAzureCredential());
+                //
+                //var secret = await client.GetSecretAsync(secretName);
+
+                
+                var keyVaultEndpoint = _configuration.GetValue<string>("AzureKeyVault:Endpoint");
+                var clientId = _configuration.GetValue<string>("AzureKeyVault:ClientId");
+                var clientSecret = _configuration.GetValue<string>("AzureKeyVault:ClientSecret");
+                var tenantId = "83a8a1a0-dfd1-43e6-976d-73889e9bc230";
+                TokenCredential tokenCredential;
+
+                var credentials = new List<TokenCredential>
+                {
+                    //new ManagedIdentityCredential(),
+                    //new EnvironmentCredential()
+                };
+
+                #if DEBUG
+                  // credentials.Add(new VisualStudioCredential());
+                  // credentials.Add(new VisualStudioCodeCredential());
+                #endif
+
+                if (!string.IsNullOrWhiteSpace(tenantId) &&
+                    !string.IsNullOrWhiteSpace(clientId) &&
+                    !string.IsNullOrWhiteSpace(clientSecret))
+                {
+                    credentials.Add(new ClientSecretCredential(tenantId, clientId, clientSecret));
+                    tokenCredential = new ChainedTokenCredential(credentials.ToArray());
+                }
+                else
+                {
+                    tokenCredential = new ChainedTokenCredential(new ManagedIdentityCredential(), new EnvironmentCredential());
+                }
+                
+                _client = new SecretClient(vaultUri: new Uri(keyVaultUrl), credential: tokenCredential);
             }
         }
 
         #endregion
+
+        public async Task<string> GetSecretValue(string keyName)
+        {
+            string secret = "";
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            //slow without ConfigureAwait(false)    
+            //keyvault should be keyvault DNS Name    
+            var secretBundle = await keyVaultClient.GetSecretAsync(Environment.GetEnvironmentVariable("keyvault") + keyName).ConfigureAwait(false);
+            secret = secretBundle.Value;
+            Console.WriteLine(secret);
+            return secret;
+        }
+
 
         #region Methods
 
